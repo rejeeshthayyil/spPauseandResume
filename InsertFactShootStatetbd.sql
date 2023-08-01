02ce3c28ed486901dff8ead5e19f2a6ca5de652d
rejeeshthayyil <93378557+rejeeshthayyil@users.noreply.github.com>
2023-08-01 15:58:07 +0100

    Add files via upload

--- InsertFactShootStatetbd.sql
+++ InsertFactShootStatetbd.sql
@@ -36,16 +36,30 @@ VALUES
 
 
 
-(4, 4, 58, 3, 3, 3, 3, 1, '2023-07-31 14:00:00', NULL, NULL, NULL, NULL, NULL, '2023-07-31 15:00:00', '2023-07-31 15:45:00', '2023-07-31 09:00:00', '2023-07-31 14:00:00',NULL, 4, '2023-07-31 15:45:00')  ,
+--(4, 4, 58, 3, 3, 3, 3, 1, '2023-07-31 14:00:00', NULL, NULL, NULL, NULL, NULL, '2023-07-31 15:00:00', '2023-07-31 15:45:00', '2023-07-31 09:00:00', '2023-07-31 14:00:00',NULL, 4, '2023-07-31 15:45:00')  ,
 
--- Fifth row (EventTypeId: 59, Shoot Initialized)
-(5, 5, 59, 5, 5, 5, 5, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5, '2023-07-31 16:00:00'),
+---- Fifth row (EventTypeId: 59, Shoot Initialized)
+--(5, 5, 59, 5, 5, 5, 5, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 5, '2023-07-31 16:00:00'),
 
--- Sixth row (EventTypeId: 57, Shoot Paused with null values in additional columns)
-(6, 6, 57, 6, 6, 6, 6, 1, '2023-07-31 17:00:00', 6, NULL, NULL, NULL, NULL, '2023-07-31 17:10:00', NULL, NULL, NULL, NULL, 6, '2023-07-31 17:15:00'),
+---- Sixth row (EventTypeId: 57, Shoot Paused with null values in additional columns)
+--(6, 6, 57, 6, 6, 6, 6, 1, '2023-07-31 17:00:00', 6, NULL, NULL, NULL, NULL, '2023-07-31 17:10:00', NULL, NULL, NULL, NULL, 6, '2023-07-31 17:15:00'),
 
--- Seventh row (EventTypeId: 57, Shoot Paused with non-null values in additional columns)
-(7, 7, 57, 7, 7, 7, 7, 1, '2023-07-31 18:00:00', 7, 'Issue 2', 'Comment 2', 2, 0, '2023-07-31 18:10:00', NULL, NULL, NULL, NULL, 7, '2023-07-31 18:15:00');
+---- Seventh row (EventTypeId: 57, Shoot Paused with non-null values in additional columns)
+--(7, 7, 57, 7, 7, 7, 7, 1, '2023-07-31 18:00:00', 7, 'Issue 2', 'Comment 2', 2, 0, '2023-07-31 18:10:00', NULL, NULL, NULL, NULL, 7, '2023-07-31 18:15:00');
+
+
+--(1000238, 7, 57, 7, 31072023, 7, 4, 1, '2023-07-31 14:00:00', 1, 'Issue 1', 'Comment 1', 2, 0, '2023-07-31 14:30:00', NULL, '2023-07-31 09:30:00', '2023-07-31 13:30:00', NULL, 7, '2023-07-31 14:30:00');
+
+
+--(1000239, 7, 58, 7, 31072023, 7, 4, 1, '2023-07-31 14:30:00', null, null, null, 2, 1, null, '2023-07-31 14:30:00.000', '2023-07-31 09:30:00', '2023-07-31 13:30:00', NULL, 7, '2023-07-31 14:30:00')
+
+--- Second row (EventTypeId: 56, Shoot Started)
+--(1000241, 2, 57, 2, 31072023, 2, 2, 1, '2023-07-31 09:30:00', 6, 'Fire alarm', 'Fire alar 6', 4, 0, '2023-07-31 10:15:00', NULL, '2023-07-31 09:30:00', '2023-07-31 13:30:00', '2023-07-31 17:30:00', 2, '2023-07-31 10:15:00'),
+
+
+--(1000242, 2, 57, 2, 31072023, 102, 2, 1, '2023-07-31 09:30:00', 6, 'Fire alarm', 'Fire alar 6', 4, 0, Null, '2023-07-31 10:30:00', '2023-07-31 09:30:00', '2023-07-31 13:30:00', '2023-07-31 17:30:00', 2, '2023-07-31 10:30:00')
+
+(1000242, 2, 57, 2, 31072023, 102, 2, 1, '2023-07-31 09:30:00', 3, 'Tech Issue', 'Tech Issue 6', 4, 0, Null, '2023-07-31 11:00:00', '2023-07-31 09:30:00', '2023-07-31 13:30:00', '2023-07-31 17:30:00', 2, '2023-07-31 11:00:00')
 
 
 select * from Report.FactShootStatetbd
@@ -53,22 +67,39 @@ select * from Report.FactShootStatetbd
 
 
 delete report.factShootstatetbd
-where factshootstateid=7 
+where factshootstateid=7
 
-55 shoottimingset
-56 shootstarted
-57 shootpaused
-58 shootresume
-59 shootinitialized
+	--EVENTS				ISSUE
+55 shoottimingset		1 Model Lateness
+56 shootstarted			2 Model Issue
+57 shootpaused			3 Tech Issue
+58 shootresume			4 H & M Issue
+						5 Shoot team lateness
+59 shootinitialized		6 Fire alarm
+						7 Oher
+
+
+select * from Report.FactShootStatetbd
 
 update Report.FactShootStatetbd
-set ExpectedMorningStartTime= '2023-07-31 09:30:00'
-where factshootstateid =3
+set ShootTimeId= 7
+where factshootstateid =8
 
 update Report.FactShootStatetbd
-set ExpectedAfternoonStartTime= '2023-07-31 13:30:00'
-where factshootstateid=8
+set ShootPausedDateTime= '2023-07-31 13:30:00.000'
+where factshootstateid=13
 
 update Report.FactShootStatetbd
 set ExpectedEveningstarttime= '2023-07-31 17:30:00'
 where EventTypeId= 56
+
+SELECT fs.StudioId,ds.issueName,st.ShootTimeName,
+    fs.ShootPausedDateTime,
+    fs.shootresumedatetime,
+    DATEDIFF(MINUTE, ShootPausedDateTime, shootresumedatetime) AS [Time Lost]
+FROM
+    report.FactShootStatetbd fs
+	join Report.DimIssues ds
+	on fs.IssuedId=ds.issueId
+	join Report.DimShootTime st 
+	on st.ShootTimeId=fs.shoottimeId
