da13f81f42d1b68bec50036054a86f56f7e5b296
rejeeshthayyil <93378557+rejeeshthayyil@users.noreply.github.com>
2023-08-01 16:51:33 +0100

    Add files via upload

--- VmPauseandResume.sql
+++ VmPauseandResume.sql
@@ -1,22 +1,23 @@
+--CREATE VIEW [Report].[ShootTimeLostView]
+--AS
 
-
-
-CREATE VIEW [Report].[ShootTimeLostView]
-AS
 SELECT
     ds.StudioName,
-    di.issueName as [Shoot Start Status],
-    st.ShootTimeName,
+    di.issueName AS [Shoot Start Status],
+    SUM(CASE WHEN st.ShootTimeName = 'AM' THEN DATEDIFF(MINUTE, fs.ShootPausedDateTime, fs.shootresumedatetime) ELSE 0 END) AS TimeLostAM,
+    SUM(CASE WHEN st.ShootTimeName = 'PM' THEN DATEDIFF(MINUTE, fs.ShootPausedDateTime, fs.shootresumedatetime) ELSE 0 END) AS TimeLostPM,
     fs.ShootPausedDateTime,
-    fs.ShootResumeDateTime,
-    DATEDIFF(MINUTE, fs.ShootPausedDateTime, fs.shootresumedatetime) AS [Time Lost]
+    fs.ShootResumeDateTime
 FROM
     report.FactShootStatetbd fs
 JOIN
     Report.DimIssues di ON fs.IssuedId = di.issueId
 JOIN
     Report.DimShootTime st ON st.ShootTimeId = fs.shoottimeId
-join 
-	Report.DimStudio ds ON ds.StudioId=fs.StudioId
-
-
+JOIN
+    Report.DimStudio ds ON ds.StudioId = fs.StudioId
+GROUP BY
+    ds.StudioName,
+    di.issueName,
+    fs.ShootPausedDateTime,
+    fs.ShootResumeDateTime;
