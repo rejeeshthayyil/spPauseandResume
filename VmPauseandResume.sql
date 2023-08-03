dc8cc9108e76224a38c4361c0a0b96668bbfd6c4
rejeeshthayyil <93378557+rejeeshthayyil@users.noreply.github.com>
2023-08-03 12:18:52 +0100

    Update VmPauseandResume.sql

--- VmPauseandResume.sql
+++ VmPauseandResume.sql
@@ -1,8 +1,9 @@
 --CREATE VIEW [Report].[ShootTimeLostView]
 --AS
-
-SELECT
+With Timelost as(
+SELECT 
     ds.StudioName,
+	st.ShootTimeName,
     di.issueName AS [Shoot Start Status],
     SUM(CASE WHEN st.ShootTimeName = 'AM' THEN DATEDIFF(MINUTE, fs.ShootPausedDateTime, fs.shootresumedatetime) ELSE 0 END) AS TimeLost_AM,
     SUM(CASE WHEN st.ShootTimeName = 'PM' THEN DATEDIFF(MINUTE, fs.ShootPausedDateTime, fs.shootresumedatetime) ELSE 0 END) AS TimeLost_PM,
@@ -16,8 +17,17 @@ JOIN
     Report.DimShootTime st ON st.ShootTimeId = fs.shoottimeId
 JOIN
     Report.DimStudio ds ON ds.StudioId = fs.StudioId
+
 GROUP BY
     ds.StudioName,
+	st.ShootTimeName,
     di.issueName,
     fs.ShootPausedDateTime,
-    fs.ShootResumeDateTime;
+    fs.ShootResumeDateTime
+	)
+
+select studioName,ShootTimeName,[Shoot Start Status], sum(timeLost_Am) as TimeLostAM, sum(TimeLost_Pm) as TimelostPM
+from timelost
+group by studioname,
+ShootTimeName,
+[Shoot Start Status]
