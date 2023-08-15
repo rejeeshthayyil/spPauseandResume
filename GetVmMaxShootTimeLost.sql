e48c39bb9e13068391d12ad145920c789452053e
rejeeshthayyil <93378557+rejeeshthayyil@users.noreply.github.com>
2023-08-15 14:28:57 +0100

    Add files via upload

---
+++ GetVmMaxShootTimeLost.sql
@@ -0,0 +1,149 @@
+USE [Manufacturing]
+GO
+
+/****** Object:  View [Report].[GetVmMaxShootTimeLost]    Script Date: 8/15/2023 1:21:39 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+--USE [Manufacturing]
+--GO
+
+--/****** Object:  View [Report].[GetVmShootTimeLost]    Script Date: 8/11/2023 9:55:04 AM ******/
+--SET ANSI_NULLS ON
+--GO
+
+--SET QUOTED_IDENTIFIER ON
+--GO
+
+--CREATE VIEW [Report].[GetVmMaxShootTimeLost]
+--AS 
+WITH Timelost AS (
+    SELECT 
+        ds.StudioName AS [Studio Name],
+        st.ShootTimeName AS [DAY],
+
+		 CASE
+               WHEN [ShootPausedDateTime] = '1900-01-01 00:00:00.000'
+                    OR sT.ShootTimeName IS NULL THEN
+                   0
+               WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00'
+                    AND sT.ShootTimeName = 'AM' THEN
+                   1
+               WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00'
+                    AND sT.ShootTimeName = 'PM' THEN
+                   1
+               WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00'
+                    AND sT.ShootTimeName = 'FD' THEN
+                   1
+               ELSE
+                   0
+           END AS [AM Shoot],
+           CASE
+               WHEN [ShootPausedDateTime] = '1900-01-01 00:00:00.000'
+                    OR [ShootPausedDateTime] IS NULL THEN
+                   0
+               WHEN CAST([ShootPausedDateTime] AS TIME) > '13:45:00'
+                    AND sT.ShootTimeName = 'PM' THEN
+                   1
+               WHEN CAST([ShootPausedDateTime] AS TIME) > '13:45:00'
+                    AND sT.ShootTimeName = 'AM' THEN
+                   1
+               WHEN CAST([ShootPausedDateTime] AS TIME) > '13:45:00'
+                    AND sT.ShootTimeName = 'FD' THEN
+                   1
+               ELSE
+                   0
+           END AS [PM Shoot],
+
+
+        di.issueName AS [ShootStartStatus],
+
+        CASE
+            WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00' AND CAST([ShootResumeDateTime] AS TIME) <= '13:45:00' THEN 
+                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootResumeDateTime] AS TIME)) - 
+                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootPausedDateTime] AS TIME))
+            WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00' AND CAST([ShootResumeDateTime] AS TIME) > '13:45:00' THEN 
+                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST('13:45:00' AS TIME)) - 
+                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootPausedDateTime] AS TIME))
+        END AS [TimeLostAM],
+
+        CASE       
+            WHEN CAST([ShootPausedDateTime] AS TIME) > '13:45:00' AND CAST([ShootResumeDateTime] AS TIME) > '13:45:00' THEN 
+                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootResumeDateTime] AS TIME)) - 
+                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootPausedDateTime] AS TIME))
+            WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00' AND CAST([ShootResumeDateTime] AS TIME) > '13:45:00' THEN 
+                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootResumeDateTime] AS TIME)) - 
+                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST('13:45:00' AS TIME))
+        END AS [TimeLostPM],
+
+        fs.ShootPausedDateTime,
+        fs.ShootResumeDateTime
+    FROM
+        report.FactShootStatetbd fs
+    JOIN
+        Report.DimIssues di ON fs.IssuedId = di.issueId
+    JOIN
+        Report.DimShootTime st ON st.ShootTimeId = fs.shoottimeId
+    JOIN
+        Report.DimStudio ds ON ds.StudioId = fs.StudioId
+
+)
+
+,Studios AS (
+    SELECT 
+        [Studio Name],
+		[DAY],
+		[AM Shoot],
+		[PM Shoot],
+        [ShootStartStatus],
+        MAX(TimeLostAM) / 60 AS TimeLostAM, 
+        MAX(TimeLostPM) / 60 AS TimeLostPM
+    FROM 
+        TimeLost
+    GROUP BY 
+        [Studio Name],[DAY], [AM Shoot],[PM Shoot],[ShootStartStatus]
+),
+StudioLevel AS (
+    SELECT 
+        [Studio Name], 
+		[DAY],
+		[ShootStartStatus],
+        ISNULL(SUM(TimeLostAM), 0) AS HighestTimeLostAM, 
+        ISNULL(SUM(TimeLostPM), 0) AS HighestTimeLostPM,
+        ISNULL(SUM(TimeLostAM), 0) + ISNULL(SUM(TimeLostPM), 0) AS CombinedTimeLost
+    FROM 
+        Studios
+    GROUP BY 
+        [Studio Name],[DAY],[ShootStartStatus]
+),
+RankedStatus AS (
+    SELECT
+        [Studio Name],
+        [DAY],
+        [ShootStartStatus],
+       -- HighestTimeLostAM,
+       -- HighestTimeLostPM,
+        SUM(HighestTimeLostAM) OVER(PARTITION BY [Studio Name]) AS TotalTimeLostAM,
+        SUM(HighestTimeLostPM) OVER(PARTITION BY [Studio Name]) AS TotalTimeLostPM,
+        ROW_NUMBER() OVER(PARTITION BY [Studio Name] ORDER BY CombinedTimeLost DESC) AS rn
+    FROM 
+        StudioLevel
+)
+SELECT 
+    [Studio Name],
+    [DAY],
+    [ShootStartStatus],
+   -- HighestTimeLostAM,
+   -- HighestTimeLostPM,
+    TotalTimeLostAM,
+    TotalTimeLostPM
+FROM 
+    RankedStatus
+WHERE
+    rn = 1;
+GO
+
+
