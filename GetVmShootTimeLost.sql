201b98708ae05b8e6d9c93607db5daae55e9f826
rejeeshthayyil <93378557+rejeeshthayyil@users.noreply.github.com>
2023-09-04 18:41:18 +0100

    Update GetVmShootTimeLost.sql

--- GetVmShootTimeLost.sql
+++ GetVmShootTimeLost.sql
@@ -1,110 +1,94 @@
-USE [Manufacturing]
+USE [Onlinestore]
 GO
 
-/****** Object:  View [Report].[GetVmIssueShootTimeLost]    Script Date: 8/15/2023 2:27:54 PM ******/
+/****** Object:  View [Report].[ShootTimeLostViewDuplicate]    Script Date: 04/09/2023 18:40:36 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
---USE [Manufacturing]
---GO
 
---/****** Object:  View [Report].[GetVmShootTimeLost]    Script Date: 8/11/2023 9:55:04 AM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE VIEW [Report].[GetVmIssueShootTimeLost]
---AS 
+CREATE VIEW [Report].[ShootTimeLostViewDuplicate]
+AS 
 WITH Timelost AS (
     SELECT 
-        ds.StudioName AS [Studio Name],
-        st.ShootTimeName AS [DAY],
-
-		 CASE
-               WHEN [ShootPausedDateTime] = '1900-01-01 00:00:00.000'
-                    OR sT.ShootTimeName IS NULL THEN
-                   0
-               WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00'
-                    AND sT.ShootTimeName = 'AM' THEN
-                   1
-               WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00'
-                    AND sT.ShootTimeName = 'PM' THEN
-                   1
-               WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00'
-                    AND sT.ShootTimeName = 'FD' THEN
-                   1
-               ELSE
-                   0
-           END AS [AM Shoot],
-           CASE
-               WHEN [ShootPausedDateTime] = '1900-01-01 00:00:00.000'
-                    OR [ShootPausedDateTime] IS NULL THEN
-                   0
-               WHEN CAST([ShootPausedDateTime] AS TIME) > '13:45:00'
-                    AND sT.ShootTimeName = 'PM' THEN
-                   1
-               WHEN CAST([ShootPausedDateTime] AS TIME) > '13:45:00'
-                    AND sT.ShootTimeName = 'AM' THEN
-                   1
-               WHEN CAST([ShootPausedDateTime] AS TIME) > '13:45:00'
-                    AND sT.ShootTimeName = 'FD' THEN
-                   1
-               ELSE
-                   0
-           END AS [PM Shoot],
-
-
+        ds.StudioName,
+        st.ShootTimeName,
         di.issueName AS [ShootStartStatus],
 
-        CASE
+      CASE
             WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00' AND CAST([ShootResumeDateTime] AS TIME) <= '13:45:00' THEN 
-                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootResumeDateTime] AS TIME)) - 
-                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootPausedDateTime] AS TIME))
+                 DATEDIFF(MINUTE, CAST([ShootPausedDateTime] AS TIME), CAST([ShootResumeDateTime] AS TIME))
             WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00' AND CAST([ShootResumeDateTime] AS TIME) > '13:45:00' THEN 
-                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST('13:45:00' AS TIME)) - 
-                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootPausedDateTime] AS TIME))
+                 DATEDIFF(MINUTE, CAST([ShootPausedDatetime] AS TIME), 
+				 CAST('13:45:00' AS TIME))
         END AS [TimeLostAM],
 
-        CASE       
+
+         CASE       
             WHEN CAST([ShootPausedDateTime] AS TIME) > '13:45:00' AND CAST([ShootResumeDateTime] AS TIME) > '13:45:00' THEN 
-                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootResumeDateTime] AS TIME)) - 
-                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootPausedDateTime] AS TIME))
+                DATEDIFF(MINUTE, CAST([ShootPausedDateTime] AS TIME), CAST([ShootResumeDateTime] AS TIME))
             WHEN CAST([ShootPausedDateTime] AS TIME) <= '13:45:00' AND CAST([ShootResumeDateTime] AS TIME) > '13:45:00' THEN 
-                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST([ShootResumeDateTime] AS TIME)) - 
-                DATEDIFF(SECOND, CAST('00:00:00' AS TIME), CAST('13:45:00' AS TIME))
+                DATEDIFF(MINUTE, CAST('13:45:00' AS TIME),CAST([ShootResumeDateTime] AS TIME))
         END AS [TimeLostPM],
+
+			 CASE
+    WHEN CAST([ShootPausedDateTime] AS TIME) > '17:30:00' AND CAST([ShootResumeDateTime] AS TIME) > '17:30:00' THEN
+        DATEDIFF(MINUTE, CAST([ShootPausedDateTime] AS TIME), CAST([ShootResumeDateTime] AS TIME))
+    WHEN CAST([ShootPausedDateTime] AS TIME) <= '17:30:00' AND CAST([ShootResumeDateTime] AS TIME) > '17:30:00' THEN
+	 DATEDIFF(MINUTE, CAST([ShootResumeDateTime] AS TIME),CAST('17:30:00' AS TIME))
+
+END AS [TimeLostEV],
+
+
 
         fs.ShootPausedDateTime,
         fs.ShootResumeDateTime
     FROM
         report.FactShootStatetbd fs
     JOIN
-        Report.DimIssues di ON fs.IssuedId = di.issueId
+        Report.DimIssue di ON fs.IssuedId = di.issueId
     JOIN
         Report.DimShootTime st ON st.ShootTimeId = fs.shoottimeId
     JOIN
         Report.DimStudio ds ON ds.StudioId = fs.StudioId
-
 )
-
---,Studios AS (
+, Studios AS (
     SELECT 
-        [Studio Name],
-		[DAY],
-		--[AM Shoot],
-		--[PM Shoot],
-        [ShootStartStatus],
-        MAX(TimeLostAM) / 60 AS TimeLostAM, 
-        MAX(TimeLostPM) / 60 AS TimeLostPM
+        studioName,
+        ShootTimeName,
+        [ShootStartStatus] ,
+        SUM(TimeLostAM)   AS TimeLostAM, 
+        SUM(TimeLostPM)   AS TimeLostPM,
+		sum(TimeLostEV)   AS TimeLostEV
     FROM 
-        TimeLost
+        Timelost
     GROUP BY 
-        [Studio Name],[DAY], [AM Shoot],[PM Shoot],[ShootStartStatus]
+        studioName, ShootTimeName, [ShootStartStatus]
+		
+	    
+)
+, StudioLevel AS (
+    SELECT 
+        studioName, 
+        SUM(TimeLostAM) AS TotalTimeLostAM, 
+        SUM(TimeLostPM) AS TotalTimeLostPM,
+		sum(TimeLostEV) AS TotalTimeLostEV
+    FROM 
+        Studios
+    GROUP BY 
+        studioName
+)
+SELECT 
+    studioName,
+    SUM(TotalTimeLostAM) AS TotalTimeLostAM,
+    SUM(TotalTimeLostPM) AS TotalTimeLostPM,
+	sum(TotalTimeLostEV) as TotalTimeLostEV
+FROM 
+    StudioLevel
+GROUP BY 
+    studioName;
 GO
 
 
